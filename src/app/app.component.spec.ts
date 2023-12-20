7eff3cb6bd6c690dc610f2820275c23e89e93306
DESKTOP-MGOVL9U\ACH <nature8051@gmail.com>
2023-12-20 11:21:38 +0800

    add router

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -7,23 +7,4 @@ describe('AppComponent', () => {
       imports: [AppComponent],
     }).compileComponents();
   });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'Travel-Map' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Travel-Map');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Travel-Map');
-  });
 });
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  isShownPassword = false;
+  showPassword() {
+    this.isShownPassword = !this.isShownPassword;
+  }
+}
