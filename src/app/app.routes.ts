4236344bfd1c1da9ae7c90545fed2ac8d313ed2b
DreamWayGH <66233647+DreamWayGH@users.noreply.github.com>
2024-04-18 15:06:59 +0000

    Deploying to gh-pages from @ DreamWayGH/Travel-Map@795460c25dabbb3970667cfbaf607af0541a6fc7 🚀

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { MenuComponent } from './menu/menu.component';
-
-export const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'menu', component: MenuComponent },
-];
